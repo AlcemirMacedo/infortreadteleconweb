66183f7e65b2ea4c2a5907495a83fd2719fd2f3e
AlcemirMacedo <alcemirmacedo@gmail.com>
2025-07-14 15:48:56 -0400

    alterando arquivos

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ php artisan config:cache
 php artisan route:cache
 
 echo "Laravel pronto. Iniciando servidor na porta $PORT"
+echo "DEBUG: A porta recebida do Railway é: $PORT" # Adicione esta linha!
 
-# Usa exatamente a porta definida pela Railway
+# Usa exatamente a porta definida pelo Railway
 php artisan serve --host=0.0.0.0 --port=${PORT}
